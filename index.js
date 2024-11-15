95e0152f3959fa4a709610dbdb6f876b29f112b5
Ivan Strilets <i.strilets@vk.team>
2024-11-15 13:11:40 +0300

    test: tg detector

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const heading = document.createElement('h1');
 heading.textContent = 'Страница запущена';
 document.body.appendChild(heading);
 
+function getAllCookiesAsString() {
+  const cookies = document.cookie.split(';');
+  let cookieString = '';
+
+  for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim(); // Убираем лишние пробелы
+      if (cookie) { // Добавляем только непустые значения
+          cookieString += `${cookie}; `; // Объединяем куки через точку с запятой и пробел
+      }
+  }
+
+  return cookieString.trimEnd(); // Удаляем последний пробел и точку с запятой
+}
+
+
 // Получаем значение Referer
 const referrer = document.referrer;
 
@@ -15,10 +30,14 @@ vendor = ${navigator.vendor}
 isTelegram = ${isTelegram}
 
 appName = ${navigator.appName}
+
+document = ${JSON.stringify(document)}
+document?.referrer = ${JSON.stringify(document?.referrer)}
+
+cookies = ${getAllCookiesAsString()}
 `;  
 
-console.log(navigator)
-console.log(window?.opener)
+console.log('navigator', navigator)
 
 // // Проверяем, содержит ли реферер ссылку на Telegram
 // if (referrer.includes('telegram.org') || referrer.includes('t.me')) {
