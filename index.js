26005e30d7d5657b48745bb389c4daadb85fd7e0
Ivan Strilets <i.strilets@vk.team>
2024-11-15 12:37:43 +0300

    test: tg detector

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ document.body.appendChild(heading);
 // Получаем значение Referer
 const referrer = document.referrer;
 
-const text = document.createElement('p');
-text.textContent = 'Страница запущена';
+const text = document.createElement('pre');
+text.textContent = `userAgent = ${navigator.userAgent};
 
+vendor = ${navigator.vendor}`;  
 
-// Проверяем, содержит ли реферер ссылку на Telegram
-if (referrer.includes('telegram.org') || referrer.includes('t.me')) {
-  text.textContent = 'Переход выполнен из Telegram';  
-} else {
-  text.textContent = 'Переход выполнен из браузера';  
-}
+console.log(navigator)
+
+// // Проверяем, содержит ли реферер ссылку на Telegram
+// if (referrer.includes('telegram.org') || referrer.includes('t.me')) {
+//   text.textContent = 'Переход выполнен из Telegram';  
+// } else {
+//   text.textContent = 'Переход выполнен из браузера';  
+// }
 
 document.body.appendChild(text);
